f7c79a4ff95ba5a400a83a291592e76c86d34f82
mina.guirguis <mina.guirguis@perficient.com>
2022-02-17 16:12:47 -0500

    created update function, setup appointment DB annotation

--- appointment/src/main/java/com/perfiecent/bcproj/appointment/model/Appointment.java
+++ appointment/src/main/java/com/perfiecent/bcproj/appointment/model/Appointment.java
@@ -2,16 +2,14 @@ package com.perfiecent.bcproj.appointment.model;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.UUID;
 
-
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name="appointments")
 public class Appointment {
 
 
@@ -19,11 +17,22 @@ public class Appointment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(name = "apptName", nullable = false)
     private String apptName;
+
+    @Column(name = "apptType", nullable = false)
     private String apptType;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "startTime", nullable = false)
     private String startTime;
+
+    @Column(name = "endTime", nullable = false)
     private String endTime;
+
+    @Column(name = "metaData", nullable = false)
     private String metaData;
 
 }
