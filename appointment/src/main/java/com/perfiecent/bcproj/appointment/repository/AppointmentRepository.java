a54bcb66e4cedbc6b3bca6b4c9ebb72b72b34f41
mina.guirguis <mina.guirguis@perficient.com>
2022-02-17 17:01:24 -0500

    saving changes

--- appointment/src/main/java/com/perfiecent/bcproj/appointment/repository/AppointmentRepository.java
+++ appointment/src/main/java/com/perfiecent/bcproj/appointment/repository/AppointmentRepository.java
@@ -4,9 +4,11 @@ package com.perfiecent.bcproj.appointment.repository;
 import com.perfiecent.bcproj.appointment.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
+
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
 
