7db95da23e54c05e1b0322146ee72a090a7280e3
mina.guirguis <mina.guirguis@perficient.com>
2022-02-28 16:16:14 -0500

    Create user JS function made

--- manager/src/main/resources/static/Login.js
+++ manager/src/main/resources/static/Login.js
@@ -1,6 +1,21 @@
 
+function createUser() {
+    const email = document.getElementById("emailTxtBox").value;
+    const password = document.getElementById("passwordTxtBox").value;
+
+        $.ajax({
+            type:"POST",
+            contentType: "application/json; charset=utf-8",
+            url: "http://localhost:8080/api/v1/login",
+            data: JSON.stringify({"email" : email.toString(), "password" : password.toString()}),
+            cache: false
+        })
+}
+
 function loginUser() {
-    localStorage.setItem("lastName", "placeholder");
-    localStorage.setItem("firstName", "placeholder1");
-    console.log("working");
+    const email = document.getElementById("emailTxtBox").value;
+    const password = document.getElementById("passwordTxtBox").value;
+
+    
+
 }
