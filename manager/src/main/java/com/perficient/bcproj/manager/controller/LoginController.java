521656cf5d539a3bf6337c78dff827ea894bdc08
mina.guirguis <mina.guirguis@perficient.com>
2022-03-01 17:29:42 -0500

    fixed cors issue

--- manager/src/main/java/com/perficient/bcproj/manager/controller/LoginController.java
+++ manager/src/main/java/com/perficient/bcproj/manager/controller/LoginController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/login")
 public class LoginController {
 
@@ -21,36 +22,42 @@ public class LoginController {
         this.loginService = loginService;
     }
 
+    @CrossOrigin
     @GetMapping("/loginRepo")
     public ResponseEntity<List<Login>> getAllLogins(){
         log.debug("Getting all logins");
         return new ResponseEntity<List<Login>>(loginService.getAllLogins(), HttpStatus.OK);
     }
 
+    @CrossOrigin
     @GetMapping("/id/{id}")
     public ResponseEntity<Login> getLoginById(@PathVariable("id") Long id){
         log.debug("Got user by id");
         return new ResponseEntity<Login>(loginService.getLoginById(id), HttpStatus.OK);
     }
 
+    @CrossOrigin
     @GetMapping("/email/{email}")
     public ResponseEntity<Login> getLoginByEmail(@PathVariable("email") String email){
         log.debug("Got user by email");
         return new ResponseEntity<Login>(loginService.getLoginByEmail(email), HttpStatus.OK);
     }
 
+    @CrossOrigin
     @PutMapping("/{id}")
     public ResponseEntity<Login> updateLogin(@PathVariable("id") long id, @RequestBody Login login){
         log.debug("Updated Login");
         return new ResponseEntity<Login>(loginService.updateLogin(login, id), HttpStatus.OK);
     }
 
+    @CrossOrigin
     @PostMapping()
     public ResponseEntity<Login> createLogin(@RequestBody Login login){
         log.debug("Created new Login");
         return new ResponseEntity<Login>(loginService.createLogin(login), HttpStatus.CREATED);
     }
 
+    @CrossOrigin
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteLogin(@PathVariable("id") long id){
         log.debug("Deleting Login");
