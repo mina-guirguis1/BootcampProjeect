b86fb8f9344cb9d6029832fffc76b489ebe18534
mina.guirguis <mina.guirguis@perficient.com>
2022-03-03 16:23:14 -0500

    Got login page working

--- manager/src/main/resources/static/Login.js
+++ manager/src/main/resources/static/Login.js
@@ -13,6 +13,15 @@ function loginUser() {
         url: "http://localhost:8080/api/v1/login/email/" + email,
         success: function (response) {
             console.log("It worked");
+            const getPassFromDB = response["password"];
+            const getUserID = response["id"];
+            if(password == getPassFromDB) {
+                //redirect
+                localStorage.setItem("userID", getUserID);
+                window.location.replace("appointments.html");
+            } else {
+               alert("The password you entered was wrong");
+            }
         },
         error: function (error) {
             console.log("It failed");
