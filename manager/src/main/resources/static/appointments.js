8473936e35ff1b6bcf9e501829992902aa6d3c26
Joshua Hammer <joshua.hammer@perficient.com>
2022-03-03 16:20:38 -0600

    Latest appt

--- manager/src/main/resources/static/appointments.js
+++ manager/src/main/resources/static/appointments.js
@@ -9,15 +9,13 @@ function editAppts(id){
 	    success : function(){
 	        document.getElementById('Id').value='new value';
 
-	    },
+	    },+
 
 	})
 }
 
 function deleteThis(id){
 				if (window.confirm('Do you really want to delete record?')) {
-					var id = $(this).attr('value');
-					var parent = $(this).parent().parent();
 					$.ajax({
 						type: "DELETE",
 						url: "http://localhost:8082/api/v1/appointments/" + id,
@@ -27,12 +25,9 @@ function deleteThis(id){
 								$(this).remove();
 							});
 							location.reload(true)
-						},
-						error: function() {
-							$('#err').html('<span style=\'color:red; font-weight: bold; font-size: 30px;\'>Error deleting record').fadeIn().fadeOut(4000, function() {
-								$(this).remove();
-							});
 						}
 					});
 				}
 }
+
+
